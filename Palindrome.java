96c02ca1df700fd114034c4f6af425ea2bc7a071
Samatha2428 <samatharaghava2428@gmail.com>
2025-03-05 13:25:10 +0530

    Palindrome Program

---
+++ Palindrome.java
@@ -0,0 +1,15 @@
+public boolean isPalindrome(int x) {
+        if(x<0)return false;
+        int temp=x;
+        int sum=0;
+        while(x>0){
+            int r=x%10;
+            sum=(sum*10)+r;
+            x=x/10;
+        }
+        if(temp==sum){
+            return true;
+        }
+        return false;
+    }
+}
